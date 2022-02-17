c01d5b6948b311ad332aae27a826758df1e03662
imanolrtega <imanol@easytechgreen.com>
2022-02-17 14:50:45 -0300

    corrected error when loading the font

--- components/Container.js
+++ components/Container.js
@@ -87,6 +87,9 @@ export default function Container(props) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍻</text></svg>"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></link>
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Imanol Ortega" />
         <meta property="og:description" content={meta.description} />
