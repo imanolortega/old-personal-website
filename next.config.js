c27ec189ce0beda57a939cdf8bb0c187f7799ad0
imanolrtega <imanol@easytechgreen.com>
2022-02-19 13:39:14 -0300

    Change next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 
-const indexSearch = require('./plugins/search-index');
-const feed = require('./plugins/feed');
+//const indexSearch = require('./plugins/search-index');
+//const feed = require('./plugins/feed');
 //const sitemap = require('./plugins/sitemap');
-const socialImages = require('./plugins/socialImages');
-
-module.exports = withPlugins([[indexSearch], [feed], /*[sitemap]*/, [socialImages]], {
+//const socialImages = require('./plugins/socialImages');
+/*
+module.exports = withPlugins([[indexSearch], [feed], /*[sitemap], [socialImages]], {
   // By default, Next.js removes the trailing slash. One reason this would be good
   // to include is by default, the `path` property of the router for the homepage
   // is `/` and by using that, would instantly create a redirect
@@ -15,7 +15,7 @@ module.exports = withPlugins([[indexSearch], [feed], /*[sitemap]*/, [socialImage
   // By enabling verbose logging, it will provide additional output details for
   // diagnostic purposes. By default is set to false.
   // verbose: true,
-
+ /*
   env: {
     WORDPRESS_GRAPHQL_ENDPOINT: process.env.WORDPRESS_GRAPHQL_ENDPOINT,
     WORDPRESS_MENU_LOCATION_NAVIGATION: process.env.WORDPRESS_MENU_LOCATION_NAVIGATION || 'PRIMARY',
@@ -29,7 +29,7 @@ module.exports = withPlugins([[indexSearch], [feed], /*[sitemap]*/, [socialImage
   },
 
 });
-
+*/
 module.exports = {
   images: {
     domains: ['secure.gravatar.com'],
