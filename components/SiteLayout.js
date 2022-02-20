199d1d34b5cf8db0723bbff389af97e5712ebf4f
imanolrtega <imanol@easytechgreen.com>
2022-02-20 14:25:42 -0300

    Fix canonical and og:url in home page

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -83,9 +83,16 @@ export default function SiteLayout(props) {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://imanol.work${router.asPath}`}
+          content={`https://imanol.work${
+            router.asPath === '/index' ? '/' : router.asPath
+          }`}
         />
-        <link rel="canonical" href={`https://imanol.work${router.asPath}`} />
+        <link
+          rel="canonical"
+          href={`https://imanol.work${
+            router.asPath === '/index' ? '/' : router.asPath
+          }`}
+        />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧉</text></svg>"
@@ -105,46 +112,45 @@ export default function SiteLayout(props) {
         )}
       </Head>
 
-
-        <div className="flex flex-col justify-center px-8">
-          <nav className="flex items-center justify-between w-full relative max-w-screen-md border-gray-300 dark:border-gray-700 mx-auto pt-8 pb-4 sm:pb-4 text-gray-900 bg-slate-100 dark:bg-slate-900 bg-opacity-60 dark:text-gray-100">
-            <div className="ml-[-0.60rem]">
-              <MobileMenu />
-              <NavItem href="/" text="Home" />
-              <NavItem href="/about" text="About" />
-              <NavItem href="/blog" text="Blog" />
-              <NavItem href="/projects" text="Projects" />
-            </div>
-            <div className="flex space-x-2">
-              <IconButton
-                theme={resolvedTheme}
-                path="https://github.com/imanolrtega"
-                icon="github"
-              />
-              <IconButton
-                theme={resolvedTheme}
-                path="https://www.linkedin.com/in/imanol-rtega/"
-                icon="linkedin"
-              />
-              <DarkModeButton
-                onHandleClick={() =>
-                  setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-                }
-                isMounted={mounted}
-                isResolvedTheme={resolvedTheme}
-              />
-            </div>
-          </nav>
-          <div className="flex justify-center max-w-screen-md mx-auto w-full">
-            <hr className="w-full border-1 border-slate-300 dark:border-gray-800 mb-8" />
+      <div className="flex flex-col justify-center px-8">
+        <nav className="flex items-center justify-between w-full relative max-w-screen-md border-gray-300 dark:border-gray-700 mx-auto pt-8 pb-4 sm:pb-4 text-gray-900 bg-slate-100 dark:bg-slate-900 bg-opacity-60 dark:text-gray-100">
+          <div className="ml-[-0.60rem]">
+            <MobileMenu />
+            <NavItem href="/" text="Home" />
+            <NavItem href="/about" text="About" />
+            <NavItem href="/blog" text="Blog" />
+            <NavItem href="/projects" text="Projects" />
           </div>
+          <div className="flex space-x-2">
+            <IconButton
+              theme={resolvedTheme}
+              path="https://github.com/imanolrtega"
+              icon="github"
+            />
+            <IconButton
+              theme={resolvedTheme}
+              path="https://www.linkedin.com/in/imanol-rtega/"
+              icon="linkedin"
+            />
+            <DarkModeButton
+              onHandleClick={() =>
+                setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+              }
+              isMounted={mounted}
+              isResolvedTheme={resolvedTheme}
+            />
+          </div>
+        </nav>
+        <div className="flex justify-center max-w-screen-md mx-auto w-full">
+          <hr className="w-full border-1 border-slate-300 dark:border-gray-800 mb-8" />
         </div>
-        <main
-          id="skip"
-          className="flex flex-col justify-center px-8 bg-slate-100 dark:bg-slate-900">
-          {children}
-          <Footer />
-        </main>
+      </div>
+      <main
+        id="skip"
+        className="flex flex-col justify-center px-8 bg-slate-100 dark:bg-slate-900">
+        {children}
+        <Footer />
+      </main>
     </div>
   );
 }
