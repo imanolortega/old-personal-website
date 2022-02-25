bc1a7fb5577fb9f3f290c35f42079eb65d1feef2
imanolrtega <imanol@easytechgreen.com>
2022-02-25 00:17:38 -0300

    Fix menu mobile markup

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -1,7 +1,7 @@
 import cn from "classnames";
 import Link from "next/link";
 import useDelayedRender from "use-delayed-render";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styles from "../styles/mobile-menu.module.css";
 
 function MenuItem({ href, delay, text }) {
@@ -16,14 +16,13 @@ function MenuItem({ href, delay, text }) {
   );
 }
 
-export default function MobileMenu() {
+export default function MobileMenu({ isMenuOpen }) {
   const menuLinks = [
     { id: "0", href: "/", text: "Home", transitionDelay: "150" },
     { id: "1", href: "/about", text: "About", transitionDelay: "200" },
     { id: "2", href: "/blog", text: "Blog", transitionDelay: "250" },
     { id: "3", href: "/projects", text: "Projects", transitionDelay: "300" },
   ];
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
     isMenuOpen,
@@ -33,32 +32,8 @@ export default function MobileMenu() {
     }
   );
 
-  function toggleMenu() {
-    if (isMenuOpen) {
-      setIsMenuOpen(false);
-      document.body.style.overflow = '';
-    } else {
-      setIsMenuOpen(true);
-      document.body.style.overflow = 'hidden';
-    }
-  }
-
-  useEffect(() => {
-    return function cleanup() {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
   return (
     <div>
-      <button
-        className={cn(styles.burger, 'visible md:hidden')}
-        aria-label="Toggle menu"
-        type="button"
-        onClick={toggleMenu}>
-        {!isMenuOpen && <MenuIcon />}
-        {isMenuOpen && <CrossIcon />}
-      </button>
       {isMenuMounted && (
         <ul
           className={cn(
@@ -77,52 +52,5 @@ export default function MobileMenu() {
         </ul>
       )}
     </div>
-  );
-}
-
-function MenuIcon() {
-  return (
-    <svg
-      className="h-5 w-5 absolute text-gray-900 dark:text-gray-100"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      >
-      <path
-        d="M2.5 7.5H17.5"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M2.5 12.5H17.5"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-function CrossIcon() {
-  return (
-    <svg
-      className="h-5 w-5 absolute text-gray-900 dark:text-gray-100"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      fill="none"
-      shapeRendering="geometricPrecision"
-      >
-      <path d="M18 6L6 18" />
-      <path d="M6 6l12 12" />
-    </svg>
   );
 }
--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -1,8 +1,11 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
+import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+
+import cn from "classnames";
+import styles from "../styles/mobile-menu.module.css";
 
 import Site from '../contexts/siteLayout';
 
@@ -62,6 +65,54 @@ function DarkModeButton({ isMounted, isResolvedTheme, onHandleClick }) {
   );
 }
 
+function MenuIcon() {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-900 dark:text-gray-100"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      >
+      <path
+        d="M2.5 7.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.5 12.5H17.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon() {
+  return (
+    <svg
+      className="h-5 w-5 absolute text-gray-900 dark:text-gray-100"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      shapeRendering="geometricPrecision"
+      >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
+    </svg>
+  );
+}
+
+
 export default function SiteLayout(props) {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
@@ -78,6 +129,24 @@ export default function SiteLayout(props) {
     type: "website",
     ...customMeta,
   };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      document.body.style.overflow = '';
+    } else {
+      setIsMenuOpen(true);
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      document.body.style.overflow = '';
+    };
+  }, []);
 
   return (
     <Site.Provider
@@ -121,9 +190,17 @@ export default function SiteLayout(props) {
         </Head>
 
         <header className="flex flex-col justify-center px-8">
+          <MobileMenu isMenuOpen={isMenuOpen} />
           <nav className="flex items-center justify-between w-full relative max-w-screen-md border-gray-300 dark:border-gray-700 mx-auto pt-8 pb-4 sm:pb-4 text-gray-900 bg-slate-100 dark:bg-slate-900 bg-opacity-60 dark:text-gray-100">
             <ul className="inline-flex ml-[-0.60rem]">
-              <MobileMenu />
+              <button
+                className={cn(styles.burger, 'visible md:hidden')}
+                aria-label="Toggle menu"
+                type="button"
+                onClick={toggleMenu}>
+                {!isMenuOpen && <MenuIcon />}
+                {isMenuOpen && <CrossIcon />}
+              </button>
               <NavItem href="/" text="Home" />
               <NavItem href="/about" text="About" />
               <NavItem href="/blog" text="Blog" />
